ac468a2436fee774f069b73f7ff7e1a4daec31c8
ThuanNguyenITVN <nguyenminhthuan2003st@gmail.com>
2024-05-10 06:51:09 +0700

    final update

--- src/controllers/companyAuthController.js
+++ src/controllers/companyAuthController.js
@@ -35,6 +35,7 @@ const companyLogin = async(req, res) => {
     try {
     // Create checkAcount function to check students account with name and age
         const { email, password } = req.body;
+        console.log(password)
         const company = await db.companies.findOne({ email: email, });
         if (!company) {
             return res.status(401).json({ message: "Invalid credentials" });
--- src/routes/message.js
+++ src/routes/message.js
@@ -5,44 +5,37 @@ const WebSocket = require("ws");
 const wss = new WebSocket.Server({ noServer: true });
 
 wss.on("connection", async (ws, req) => {
-    const data = getUserIdFromRequest(req);
-    // console.log("User connected with ID:", data.senderId);
-    ws.userId = data.senderId;
-
-    await loadMessagesFromDB(data.senderId, data.receiverId,ws);
+    const userId = getUserIdFromRequest(req);
+    console.log("User connected with ID:", userId);
+    ws.userId = userId;
 
     ws.on("message", async (message) => {
         try {
             const parsedMessage = JSON.parse(message);
-            // if (parsedMessage.type === "load_messages") {
-            //     const { senderId, receiverId } = parsedMessage;
-            //     const messages = await loadMessagesFromDB(senderId, receiverId);
-            //     messages.forEach((message) => {
-            //         broadcastMessage(message, ws);
-            //     });
-            // } else 
-            if (data.senderId === parsedMessage.sender) {
-                const savedMessage = await saveMessageToDB(parsedMessage);
-                broadcastMessage(savedMessage, ws);
+            if (parsedMessage.type === "load_messages") {
+                const { senderId, receiverId } = parsedMessage;
+                const messages = await loadMessagesFromDB(senderId, receiverId);
+                messages.forEach((message) => {
+                    broadcastMessage(message, ws);
+                });
+            } else {
+                if (userId === parsedMessage.sender) {
+                    const savedMessage = await saveMessageToDB(parsedMessage);
+                    broadcastMessage(savedMessage, ws);
+                } else {
+                    console.error("Unauthorized message:", parsedMessage);
+                }
             }
         } catch (error) {
             console.error("Error handling message:", error);
         }
-    });
-    ws.on("close", () => {
-        // console.log("WebSocket connection closed for user:", ws.userId);
-        // ws.close()
     });
 });
 
 function getUserIdFromRequest(req) {
     try {
         const urlParams = new URLSearchParams(req.url.split("?")[1]);
-        data = {
-            senderId: urlParams.get("senderId"),
-            receiverId: urlParams.get("receiverId")
-        }
-        return data
+        return urlParams.get("user-id");
     } catch (error) {
         console.error("Error extracting user ID from request:", error);
         return null;
@@ -68,7 +61,7 @@ async function saveMessageToDB(message) {
 
 const broadcastMessage = async (message, sender) => {
     for (const client of wss.clients) {
-        if (client !== sender && client.readyState === WebSocket.OPEN) {
+        if (client === sender && client.readyState === WebSocket.OPEN) {
             // Kiểm tra nếu người nhận không phải là người gửi ban đầu
             if (client.userId === message.sender || client.userId === message.receiver) {
                 client.send(JSON.stringify({
@@ -77,7 +70,7 @@ const broadcastMessage = async (message, sender) => {
                     receiver: message.receiver,
                     timestamp: new Date(),
                     _id: message._id,
-                }));
+                })); 
                 break;
             }
         }
@@ -85,19 +78,15 @@ const broadcastMessage = async (message, sender) => {
 };
 
 
-async function loadMessagesFromDB(senderId, receiverId,ws) {
+async function loadMessagesFromDB(senderId, receiverId) {
     try {
-        // console.log(receiverId)
         const messages = await db.messages.find({
             $or: [
                 { sender: senderId, receiver: receiverId },
                 { sender: receiverId, receiver: senderId },
             ],
         }).toArray();
-        // console.log(messages)
-        messages.forEach((message) => {
-            broadcastMessage(message, ws);
-        })
+        console.log(messages)
         return messages;
     } catch (error) {
         console.error("Error loading messages from DB:", error);
