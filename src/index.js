b95390c7f9864aacafedcb8827514beb1e826dff
ThuanNguyenITVN <nguyenminhthuan2003st@gmail.com>
2024-09-19 20:44:36 +0700

    update message

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ app.use("/messages", messageRouter);
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://my-blog-fronend.vercel.app", // Cho phép kết nối từ frontend
+        origin: "http://localhost:3000", // Cho phép kết nối từ frontend
         methods: ["GET", "POST"],
     },
 });
